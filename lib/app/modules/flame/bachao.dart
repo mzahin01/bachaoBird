88d1f505744cac1161d72605d70c5da97b139c2f
mzahin01 <minhazahin@gmail.com>
2025-05-17 22:46:38 +0600

    bird going up and down based on tap

--- lib/app/modules/flame/bachao.dart
+++ lib/app/modules/flame/bachao.dart
@@ -1,13 +1,14 @@
 import 'package:flame/components.dart';
+import 'package:flame/events.dart';
 import 'package:flame/game.dart';
+import 'package:flame/input.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(GameWidget(game: FlappyGame()));
-}
-
-class FlappyGame extends FlameGame {
+class FlappyGame extends FlameGame with TapDetector {
   late SpriteComponent bird;
+  double velocityY = 0.0;
+  final double gravity = 1200; // pixels per second²
+  final double flapStrength = -500; // upward velocity
 
   @override
   Color backgroundColor() => const Color(0xFF87CEEB);
@@ -18,9 +19,27 @@ class FlappyGame extends FlameGame {
     bird =
         SpriteComponent()
           ..sprite = birdSprite
-          ..size = Vector2(50, 50) // Width, height
-          ..position = size / 2; // Center of the screen
-
+          ..size = Vector2(50, 50)
+          ..position = Vector2(size.x / 4, size.y / 2);
     add(bird);
+  }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+
+    velocityY += gravity * dt;
+    bird.position.y += velocityY * dt;
+
+    // Prevent bird from going below the screen
+    if (bird.position.y > size.y - bird.size.y) {
+      bird.position.y = size.y - bird.size.y;
+      velocityY = 0;
+    }
+  }
+
+  @override
+  void onTapDown(TapDownInfo info) {
+    velocityY = flapStrength;
   }
 }
